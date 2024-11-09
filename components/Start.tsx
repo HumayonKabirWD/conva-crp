38d5849732e9d9dd4a373880b5a68d2c7e76f7bc
kabir <kaabir24124@gmail.com>
2024-11-09 19:28:48 +0600

    client part

--- components/Start.tsx
+++ components/Start.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import bg from "../public/bg.png";
 import logo from "../public/icon.png";
 import Link from "next/link";
-import Seconde from './Seconde';
 
 
 
@@ -13,7 +12,7 @@ const Start = () => {
       <div className="flex">
         <Image src={bg} alt="bg" className="absolute"/>
 
-      <Link href="/" className="absolute mt-20 ml-20">
+      <Link href="/" className="absolute mt-20 ml-20 scale-up1">
         <div className=" flex items-center">
           <Image src={logo} height={70} width={70} alt="logo"/>
           <p className="mt-5 text-xl font-semibold text-[#C43B92]">CONVA-CRP</p>   
@@ -21,15 +20,15 @@ const Start = () => {
         <p className="tracking-widest font-mono ml-20 text-gray-400">TEC ICN</p>
       </Link>
 
-      <div className="flex absolute right-5 top-5 gap-4">
+      <div className="flex absolute right-5 top-5 gap-4 scale-up1">
         <Link className="bg-[#C43B92] py-2 px-4 rounded-full font-bold hover:scale-105" href="/">Home</Link>
         <Link className="bg-[#C43B92] py-2 px-4 rounded-full font-bold hover:scale-105" href="/">Service</Link>
         <Link className="bg-[#C43B92] py-2 px-4 rounded-full font-bold hover:scale-105" href="/">About</Link>
       </div>
 
       <div className="flex flex-col items-center justify-center absolute mt-72 text-center">
-        <h1 className="mx-0 lg:mx-20 text-balance text-6xl md:text-6xl lg:text-8xl uppercase italic font-bold text-[#C43B92] scale-up">describing the future</h1>
-        <button className="mt-20 bg-[#C43B92] py-2 px-5 hover:scale-105 rounded-md font-bold ">Learn More</button>
+        <h1 className="mx-0 lg:mx-20 text-balance text-6xl md:text-6xl lg:text-8xl uppercase italic font-bold text-[#C43B92] scale-up2">describing the future</h1>
+        <button className="mt-20 bg-[#C43B92] py-2 px-5 hover:scale-105 rounded-md font-bold scale-up3">Learn More</button>
       </div>
 
     </div>
--- components/Third.tsx
+++ components/Third.tsx
@@ -6,55 +6,104 @@ import p1 from '../public/pg-1.jpg'
 import p2 from '../public/pg-2.jpg'
 import p3 from '../public/pg-3.jpg'
 
+
 const Third = () => {
 
 
   return (
     <div>
-        <div className="flex items-center justify-start">
-            <h1 className='text-4xl bg-[#e8ee40] px-3 text-black font-bold italic text-balance'>What{"'"}s so spacial about our poll ?</h1>
+        <div className="flex ">
+            <p className='text-4xl uppercase py-2 text-black bg-[#e8ee40] px-5 font-bold italic'>What{"'"}s so  spachall </p>
+        </div>
+        <div className="flex ">
+            <p className='text-4xl uppercase text-black bg-[#e8ee40] px-4 font-bold italic'>in our poole ?</p>
         </div>
         {/* image 3 */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 mt-20 mb-10 items-center justify-center mx-10 gap-y-10 gap-x-10">
-
-            <div className="flex flex-col ">
+        <div className="flex-col flex lg:flex-row  items-center justify-center mt-10 lg:mt-20 gap-20 mb-10">
+            
+            <div className="flex flex-col">
+                <div className="h-56 w-56 rounded-full relative overflow-hidden ml-5">
                 <Image 
-                src={p1} 
-                alt='person'
-                width={300} 
-                height={300} 
-                className="scale-up  rounded-full"/>
-
-                <div className='h-1 w-10 bg-slate-200 rounded-xl'/>
+                    src={p1} 
+                    alt='person'
+                    width={300} 
+                    height={300} 
+                    className="scale-animation rounded-full"/>
+                </div>
+                <div className='h-1 w-10 bg-slate-200 rounded-xl mt-5'/>
                 <h2 className='text-xl font-medium mt-5 text-[#e8ee40] uppercase italic'>Locator feature</h2>
-                <p className='mt-3 text-sm text-gray-400'>A feature that allows users to track phone calls on the web. For example, the user may only see the first few digits of a phone number until they click a link to view the full number</p>
+                <p className='mt-3 text-sm text-gray-400 text-balance'>A feature that allows users to track phone calls on the web. For example, the user may only see the first few digits of a phone number until they click a link to view the full number</p>
             </div>
-            <div className="flex flex-col ">
-                <Image 
-                src={p2}
-                alt='person'
-                width={300} 
-                height={300} 
-                className='scale-up rounded-full'/>
 
-                <div className='h-1 w-10 bg-slate-200 rounded-xl'/>
-                <h2 className='text-xl font-medium mt-5 text-[#e8ee40] uppercase italic'>Locator feature</h2>
-                <p className='mt-3 text-sm text-gray-400'>A feature that allows users to track phone calls on the web. For example, the user may only see the first few digits of a phone number until they click a link to view the full number</p>
+            <div className="flex flex-col">
+                <div className="h-56 w-56 rounded-full relative overflow-hidden ml-5">
+                <Image 
+                    src={p2} 
+                    alt='person'
+                    width={300} 
+                    height={300} 
+                    className="scale-animation rounded-full"/>
+                </div>
+                <div className='h-1 w-10 bg-slate-200 rounded-xl mt-5'/>
+                <h2 className='text-xl font-medium mt-5 text-[#e8ee40] uppercase italic'>anonymous setting</h2>
+                <p className='mt-3 text-sm text-gray-400 text-balance'>When you choose private mode this gives you the option to browse LinkedIn Profiles anonymously. When activated, the LinkedIn profiles you view can not see that its you viewing</p>
             </div>
-            <div className="flex flex-col ">
-                <Image 
-                src={p3} 
-                alt='person'
-                width={300} 
-                height={300} 
-                className='scale-up rounded-full'/>
 
-                <div className='h-1 w-10 bg-slate-200 rounded-xl'/>
-                <h2 className='text-xl font-medium mt-5 text-[#e8ee40] uppercase italic'>Locator feature</h2>
-                <p className='mt-3 text-sm text-gray-400'>A feature that allows users to track phone calls on the web. For example, the user may only see the first few digits of a phone number until they click a link to view the full number</p>
+            <div className="flex flex-col">
+                <div className="h-56 w-56 rounded-full relative overflow-hidden ml-5">
+                <Image 
+                    src={p3} 
+                    alt='person'
+                    width={300} 
+                    height={300} 
+                    className="scale-animation rounded-full"/>
+                </div>
+                <div className='h-1 w-10 bg-slate-200 rounded-xl mt-5'/>
+                <h2 className='text-xl font-medium mt-5 text-[#e8ee40] uppercase italic'>freemium benifites</h2>
+                <p className='mt-3 text-sm text-gray-400 text-balance'>our teem that allows users to track phone calls on the web. For example, the user may only see the first few digits of a phone number until they click a link to view the full number</p>
             </div>
 
         </div>
+        {/* the ups and down component */}
+        <div className="flex items-center justify-end mt-20 mr-10 ">
+            <h1 className='text-4xl py-1 italic uppercase px-4 font-bold bg-[#6cec94] text-black'>why choose us ?</h1>
+        </div>
+        <div className="flex-col flex lg:flex-row  items-center justify-center gap-20 ">
+            {/* 1st  */}
+            <div className="flex flex-col items-center justify-end w-72 ">
+                <p className='text-right mr-16 text-2xl  mt-5 text-[#6cec94] font-bold uppercase italic '>coustomized  feature</p>
+                <p className=' flex text-right'>This is a best option <br /> to make your own </p>
+            <div className="flex ml-16 mt-10">
+                <div className="h-4 w-7 rounded-tl-md rounded-br-md bg-gradient-to-tr from-blue-400 to-white -rotate-45"/>
+                <div className="h-4 w-7 rounded-tl-md rounded-br-md  bg-gradient-to-tr from-blue-400 to-pink-300 -rotate-45"/>
+                <div className="h-4 w-7 rounded-tl-md rounded-br-md  bg-gradient-to-tr from-pink-300 to-pink-800 -rotate-45"/>
+            </div>
+            </div>
+            {/* 2nd */}
+            <div className="flex flex-col items-center justify-end w-72 mt-40">
+                <p className='text-right text-2xl  mt-5 text-[#6cec94] font-bold uppercase italic '>ease of use</p>
+                <p className='text-right mr-6'>Unlike your best option to <br /> make your own </p>
+            <div className="flex ml-16 mt-10">
+                <div className="h-4 w-7 rounded-tl-md rounded-br-md bg-gradient-to-tr from-blue-400 to-white -rotate-45"/>
+                <div className="h-4 w-7 rounded-tl-md rounded-br-md  bg-gradient-to-tr from-blue-400 to-pink-300 -rotate-45"/>
+                <div className="h-4 w-7 rounded-tl-md rounded-br-md  bg-gradient-to-tr from-pink-300 to-pink-800 -rotate-45"/>
+            </div>
+            </div>
+            {/* 3rd */}
+            <div className="flex flex-col items-center justify-end w-72 mt-80">
+                <p className='text-right text-2xl  mt-5 text-[#6cec94] font-bold uppercase italic '>24/7 support</p>
+                <p className='text-right mr-6'>You can take our support <br />  for sny time </p>
+            <div className="flex ml-16 mt-10">
+                <div className="h-4 w-7 rounded-tl-md rounded-br-md bg-gradient-to-tr from-blue-400 to-white -rotate-45"/>
+                <div className="h-4 w-7 rounded-tl-md rounded-br-md  bg-gradient-to-tr from-blue-400 to-pink-300 -rotate-45"/>
+                <div className="h-4 w-7 rounded-tl-md rounded-br-md  bg-gradient-to-tr from-pink-300 to-pink-800 -rotate-45"/>
+            </div>
+            </div>
+
+        </div>
+
+        {/* clien agree part */}
+        <div className=""></div>
     </div>
     )
 };
